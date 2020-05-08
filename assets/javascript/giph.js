97506c001f739af1595b6767d0ea8738cf5d4cb9
mcruz <mcruzpr86@gmail.com>
2020-05-08 13:50:04 -0400

    buttons placed

--- assets/javascript/giph.js
+++ assets/javascript/giph.js
@@ -1,6 +1,19 @@
 //Javascript
 
-let influentialPpl {
-    
-    
-}
+let starWars = [
+    'Master Yoda',
+    'Obi-Wan Kenobi',
+    'Qui Gon Jinn ',
+    'Anakin Skywalker',
+    'Darth Vader',
+    'Luke Skywalker',
+    'Princess Leah',
+    'Darth Sidious',
+    'Darth Revan',
+    'Darth Maul',
+    'Rey',
+    'Kylo Ren',
+    'BB-8',
+];
+
+
