586acdb8f05bea6d7084229c7338d9991e40c75f
mcruz <mcruzpr86@gmail.com>
2020-05-08 15:16:46 -0400

    update

--- assets/javascript/giph.js
+++ assets/javascript/giph.js
@@ -1,6 +1,6 @@
 //Javascript
 
-let starWars = [
+let starWarsCharacters = [
     'Master Yoda',
     'Obi-Wan Kenobi',
     'Qui Gon Jinn ',
@@ -16,4 +16,33 @@ let starWars = [
     'BB-8',
 ];
 
+// Adding click event listen listener to all buttons
+$("button").on("click", function() {
+    //grabs and stores the characters type value from the button
+    let cast = $(this).attr("characters");
+
+    let queryURL = "https://api.giphy.com/v1/gifs/search?q=" + cast
+     + "&api_key= 4EDRtUmmJKZKDA1pCiiVlPhQEGz4096l";
+
+
+
+     $.ajax({
+        url:queryURL,
+        method: "GET"
+    })
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
 
